739b7363514a00fd24602947e7357e828050916e
Duralei <90495632+Duralei@users.noreply.github.com>
2021-12-09 16:59:47 +0300

    PayPal Api

--- ShoppingCart/Infrastructure/SmallCartViewComponent.cs
+++ ShoppingCart/Infrastructure/SmallCartViewComponent.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShoppingCart.Models;
 using ShoppingCart.ViewModel;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ShoppingCart.Infrastructure
 {
